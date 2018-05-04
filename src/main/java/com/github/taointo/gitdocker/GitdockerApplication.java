7d488c8a19a139c2beb4143b5fd30a9a0ad02448
yangtao <yangtao4@minshenglife.com>
2018-05-04 09:50:53 +0800

    创建项目

---
+++ src/main/java/com/github/taointo/gitdocker/GitdockerApplication.java
@@ -0,0 +1,12 @@
+package com.github.taointo.gitdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitdockerApplication.class, args);
+	}
+}
